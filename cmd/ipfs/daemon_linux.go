41f6eeadfdc442e2960c7a91331860ce4c2e8759
Steven Allen <steven@stebalien.com>
2019-09-24 20:30:37 -0700

    systemd: add notify support
    That way, we can reliably know when the daemon is ready.

--- cmd/ipfs/daemon_linux.go
+++ cmd/ipfs/daemon_linux.go
@@ -4,7 +4,11 @@ package main
 
 import (
 <<<<<<< HEAD
+<<<<<<< HEAD
 	daemon "github.com/coreos/go-systemd/v22/daemon"
+=======
+	daemon "github.com/coreos/go-systemd/daemon"
+>>>>>>> systemd: add notify support
 =======
 	daemon "github.com/coreos/go-systemd/daemon"
 >>>>>>> systemd: add notify support
