582ab23b0f2c1fb584f9919fd3c2bdaa6081dceb
Steven Allen <steven@stebalien.com>
2020-02-10 13:09:00 -0800

    fix(plugin): check log error

--- plugin/plugins/peerlog/peerlog.go
+++ plugin/plugins/peerlog/peerlog.go
@@ -2,84 +2,24 @@ package peerlog
 
 import (
 	"fmt"
-<<<<<<< HEAD
-<<<<<<< HEAD
-	"sync/atomic"
-	"time"
-=======
->>>>>>> feat: add peerlog plugin
-=======
->>>>>>> feat: add peerlog plugin
 
 	core "github.com/ipfs/go-ipfs/core"
 	plugin "github.com/ipfs/go-ipfs/plugin"
 	logging "github.com/ipfs/go-log"
-<<<<<<< HEAD
-<<<<<<< HEAD
-	event "github.com/libp2p/go-libp2p-core/event"
 	network "github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"
-	"go.uber.org/zap"
-=======
-	network "github.com/libp2p/go-libp2p-core/network"
->>>>>>> feat: add peerlog plugin
-=======
-	network "github.com/libp2p/go-libp2p-core/network"
->>>>>>> feat: add peerlog plugin
 )
 
 var log = logging.Logger("plugin/peerlog")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-type eventType int
-
-var (
-	// size of the event queue buffer
-	eventQueueSize = 64 * 1024
-	// number of events to drop when busy.
-	busyDropAmount = eventQueueSize / 8
-)
-
-const (
-	eventConnect eventType = iota
-	eventIdentify
-)
-
-type plEvent struct {
-	kind eventType
-	peer peer.ID
-}
-
-=======
->>>>>>> feat: add peerlog plugin
-=======
->>>>>>> feat: add peerlog plugin
 // Log all the PeerIDs we see
 //
 // Usage:
 //   GOLOG_FILE=~/peer.log IPFS_LOGGING_FMT=json ipfs daemon
 // Output:
 //   {"level":"info","ts":"2020-02-10T13:54:26.639Z","logger":"plugin/peerlog","caller":"peerlog/peerlog.go:51","msg":"connected","peer":"QmS2H72gdrekXJggGdE9SunXPntBqdkJdkXQJjuxcH8Cbt"}
-<<<<<<< HEAD
-<<<<<<< HEAD
-//   {"level":"info","ts":"2020-02-10T13:54:59.095Z","logger":"plugin/peerlog","caller":"peerlog/peerlog.go:56","msg":"identified","peer":"QmS2H72gdrekXJggGdE9SunXPntBqdkJdkXQJjuxcH8Cbt","agent":"go-ipfs/0.5.0/"}
-//
-type peerLogPlugin struct {
-	droppedCount uint64
-	events       chan plEvent
-}
-=======
 //   {"level":"info","ts":"2020-02-10T13:54:59.095Z","logger":"plugin/peerlog","caller":"peerlog/peerlog.go:56","msg":"disconnected","peer":"QmS2H72gdrekXJggGdE9SunXPntBqdkJdkXQJjuxcH8Cbt"}
 //
 type peerLogPlugin struct{}
->>>>>>> feat: add peerlog plugin
-=======
-//   {"level":"info","ts":"2020-02-10T13:54:59.095Z","logger":"plugin/peerlog","caller":"peerlog/peerlog.go:56","msg":"disconnected","peer":"QmS2H72gdrekXJggGdE9SunXPntBqdkJdkXQJjuxcH8Cbt"}
-//
-type peerLogPlugin struct{}
->>>>>>> feat: add peerlog plugin
 
 var _ plugin.PluginDaemonInternal = (*peerLogPlugin)(nil)
 
@@ -99,140 +39,16 @@ func (*peerLogPlugin) Version() string {
 }
 
 // Init initializes plugin
-<<<<<<< HEAD
-<<<<<<< HEAD
-func (pl *peerLogPlugin) Init(*plugin.Environment) error {
-	pl.events = make(chan plEvent, eventQueueSize)
-	return nil
-}
-
-func (pl *peerLogPlugin) collectEvents(node *core.IpfsNode) {
-	ctx := node.Context()
-
-	busyCounter := 0
-	dlog := log.Desugar()
-	for {
-		// Deal with dropped events.
-		dropped := atomic.SwapUint64(&pl.droppedCount, 0)
-		if dropped > 0 {
-			busyCounter++
-
-			// sleep a bit to give the system a chance to catch up with logging.
-			select {
-			case <-time.After(time.Duration(busyCounter) * time.Second):
-			case <-ctx.Done():
-				return
-			}
-
-			// drain 1/8th of the backlog backlog so we
-			// don't immediately run into this situation
-			// again.
-		loop:
-			for i := 0; i < busyDropAmount; i++ {
-				select {
-				case <-pl.events:
-					dropped++
-				default:
-					break loop
-				}
-			}
-
-			// Add in any events we've dropped in the mean-time.
-			dropped += atomic.SwapUint64(&pl.droppedCount, 0)
-
-			// Report that we've dropped events.
-			dlog.Error("dropped events", zap.Uint64("count", dropped))
-		} else {
-			busyCounter = 0
-		}
-
-		var e plEvent
-		select {
-		case <-ctx.Done():
-			return
-		case e = <-pl.events:
-		}
-
-		peerID := zap.String("peer", e.peer.Pretty())
-
-		switch e.kind {
-		case eventConnect:
-			dlog.Info("connected", peerID)
-		case eventIdentify:
-			agent, err := node.Peerstore.Get(e.peer, "AgentVersion")
-			switch err {
-			case nil:
-			case peerstore.ErrNotFound:
-				continue
-			default:
-				dlog.Error("failed to get agent version", zap.Error(err))
-				continue
-			}
-
-			agentS, ok := agent.(string)
-			if !ok {
-				continue
-			}
-			dlog.Info("identified", peerID, zap.String("agent", agentS))
-		}
-	}
-}
-
-func (pl *peerLogPlugin) emit(evt eventType, p peer.ID) {
-	select {
-	case pl.events <- plEvent{kind: evt, peer: p}:
-	default:
-		atomic.AddUint64(&pl.droppedCount, 1)
-	}
-}
-
-func (pl *peerLogPlugin) Start(node *core.IpfsNode) error {
-	// Ensure logs from this plugin get printed regardless of global IPFS_LOGGING value
-	if err := logging.SetLogLevel("plugin/peerlog", "info"); err != nil {
-		return fmt.Errorf("failed to set log level: %w", err)
-	}
-
-	sub, err := node.PeerHost.EventBus().Subscribe(new(event.EvtPeerIdentificationCompleted))
-	if err != nil {
-		return fmt.Errorf("failed to subscribe to identify notifications")
-	}
-
-	var notifee network.NotifyBundle
-	notifee.ConnectedF = func(net network.Network, conn network.Conn) {
-		pl.emit(eventConnect, conn.RemotePeer())
-	}
-	node.PeerHost.Network().Notify(&notifee)
-
-	go func() {
-		defer sub.Close()
-		for e := range sub.Out() {
-			switch e := e.(type) {
-			case event.EvtPeerIdentificationCompleted:
-				pl.emit(eventIdentify, e.Peer)
-			}
-		}
-	}()
-
-	go pl.collectEvents(node)
-
-=======
-func (*peerLogPlugin) Init(*plugin.Environment) error {
-=======
 func (*peerLogPlugin) Init(*plugin.Environment) error {
 	fmt.Println("peerLogPlugin enabled - PeerIDs will be logged")
->>>>>>> feat: add peerlog plugin
 	return nil
 }
 
 func (*peerLogPlugin) Start(node *core.IpfsNode) error {
 	// Ensure logs from this plugin get printed regardless of global IPFS_LOGGING value
-<<<<<<< HEAD
 	if err := logging.SetLogLevel("plugin/peerlog", "info"); err != nil {
 		return fmt.Errorf("failed to set log level: %w", err)
 	}
-=======
-	logging.SetLogLevel("plugin/peerlog", "info")
->>>>>>> feat: add peerlog plugin
 	var notifee network.NotifyBundle
 	notifee.ConnectedF = func(net network.Network, conn network.Conn) {
 		log.Infow("connected",
@@ -245,10 +61,6 @@ func (*peerLogPlugin) Start(node *core.IpfsNode) error {
 		)
 	}
 	node.PeerHost.Network().Notify(&notifee)
-<<<<<<< HEAD
->>>>>>> feat: add peerlog plugin
-=======
->>>>>>> feat: add peerlog plugin
 	return nil
 }
 
