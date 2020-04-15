2fd5334899e99097d13a7bee60ecf0d370a5c03b
Steven Allen <steven@stebalien.com>
2020-04-14 20:40:45 -0700

    feat: add autonat config options
    1. Enable AutoNATService on _all_ nodes by default. If it's an issue, we can
    disable it in RC3 but this will give us the best testing results.
    2. Expose options to configure AutoNAT rate limiting.

--- test/sharness/t0185-autonat.sh
+++ test/sharness/t0185-autonat.sh
@@ -11,8 +11,6 @@ test_description="Test autonat"
 test_init_ipfs
 
 test_expect_success "enable autonat" '
-<<<<<<< HEAD
-<<<<<<< HEAD
   ipfs config AutoNAT.ServiceMode enabled
 '
 
@@ -22,12 +20,6 @@ test_kill_ipfs_daemon
 
 test_expect_success "enable autonat" '
   ipfs config AutoNAT.ServiceMode disabled
-=======
-  ipfs config --json Swarm.EnableAutoNATService true
->>>>>>> test(sharness): make sure we can actually enable autonat
-=======
-  ipfs config --json Swarm.EnableAutoNATService true
->>>>>>> test(sharness): make sure we can actually enable autonat
 '
 
 test_launch_ipfs_daemon
