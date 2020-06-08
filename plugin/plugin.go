ff2d07f8a5581f90db869f4627d02eb2f1091a7c
Walter Beegle <walterbeegle@gmail.com>
2020-06-08 05:55:39 -0400

    plugin

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -5,24 +5,11 @@ type Environment struct {
 	// Path to the IPFS repo.
 	Repo string
 
-<<<<<<< HEAD
-	// The plugin's config, if specified in the
-	// Plugins.Plugins["plugin-name"].Config field of the user's go-ipfs
-	// config. See docs/plugins.md for details.
-	//
-	// This is an arbitrary JSON-like object unmarshaled into an interface{}
-	// according to https://golang.org/pkg/encoding/json/#Unmarshal.
-	Config interface{}
-}
-
-// Plugin is the base interface for all kinds of go-ipfs plugins
-=======
 	// The plugin's config, if specified.
 	Config interface{}
 }
 
 // Plugin is base interface for all kinds of go-ipfs plugins
->>>>>>> plugins: add support for plugin configs
 // It will be included in interfaces of different Plugins
 //
 // Optionally, Plugins can implement io.Closer if they want to
