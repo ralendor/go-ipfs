CEPBEP/ipfs

namecache: add debug log on pin updates
License: MIT
Signed-off-by: vyzo <vyzo@hackzen.org>

namecache/namecache.go
@@ -196,6 +196,8 @@ func (nc *nameCache) resolveAndUpdate(ctx context.Context, name string, cid *cid
 
 	defer nc.bstore.PinLock().Unlock()
 
+	log.Debugf("Updating pin %s -> %s", cid.String(), ncid.String())
+
 	err = nc.pinning.Update(ctx, cid, ncid, true)
 	if err != nil {
 		return cid, err

ralendor/go-ipfs

namecache/namecache.go
@@ -196,6 +196,8 @@ func (nc *nameCache) resolveAndUpdate(ctx context.Context, name string, c cid.Ci
 
 	defer nc.bstore.PinLock().Unlock()
 
+	log.Debugf("Updating pin %s -> %s", c.String(), ncid.String())
+
 	err = nc.pinning.Update(ctx, c, ncid, true)
 	if err != nil {
 		return c, err